072b5fc32aa68ce8fe000c5cc35e527a4ee08cdc
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-05-27 22:14:02 -0400

    fixed up arrow position

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -177,10 +177,11 @@ const ArrowUp = styled.div`
     cursor: pointer;
     font-size: 1.3rem;
     font-weight: 700;
-    margin-top: 2rem;
+    margin-top: 1rem;
     @media (max-width: 650px) {
-        position: absolute;
+        position: fixed;
+        bottom: 15rem;
         right: 3rem;
-        top: 16rem;
+        z-index: 999;
     }
 `;
